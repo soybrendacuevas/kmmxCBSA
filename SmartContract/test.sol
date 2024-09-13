c27e2bf8a4387e3a28b60c0b993f375b6e659ff9
Brenda Cuevas <37563484+soybrendacuevas@users.noreply.github.com>
2024-09-13 12:12:42 -0600

    Update test.sol

--- SmartContract/test.sol
+++ SmartContract/test.sol
@@ -1,1 +1,21 @@
+pragma solidity ^0.8.0;
 
+contract ProductoInteligente {
+    string public nombre;
+    uint public precio;
+    address public propietario;
+
+    event ProductoCreado(string nombre, uint precio, address propietario);
+
+    constructor(string memory _nombre, uint _precio) {
+        nombre = _nombre;
+        precio = _precio;
+        propietario = msg.sender;
+        emit ProductoCreado(_nombre, _precio, msg.sender);
+    }
+
+    function transferirPropiedad(address _nuevoPropietario) public {
+        require(msg.sender == propietario, "Solo el propietario puede transferir el producto");
+        propietario = _nuevoPropietario;
+    }
+}
